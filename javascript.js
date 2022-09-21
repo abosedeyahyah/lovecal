447b58f0c5adac27de4b0f42a7cef438f243de52
abosedeyahyah <108008221+abosedeyahyah@users.noreply.github.com>
2022-09-21 02:41:37 -0700

    update the code

--- javascript.js
+++ javascript.js
@@ -1,8 +1,24 @@
 //enter your love calculation
-prompt("enter first name")
+/**prompt("enter first name")
 prompt("enter the other name")
-alert(Math.round(Math.random() * 100)+1  +"% love");
+alert(Math.round(Math.random() * 100)+1  +"% love");**/
 
+
+var firstname = prompt("what is your name")
+var secondname = prompt("what is the other name")
+let lovescore =(Math.round(Math.random() * 100) + 1);
+
+if (lovescore > 70) {
+  alert("your lovescore is "  +  lovescore  + "%. your love is excellet");
+}
+if (lovescore < 70 && lovescore > 50) {
+  alert("your lovescore is " + lovescore + "% your love is average");
+}
+if (lovescore < 50) {
+ alert("your lovescore is  " + lovescore + " %.your love is poor "); 
+} else {
+ alert("your lovescore is " + lovescore + "%."); 
+}
 
  
 
